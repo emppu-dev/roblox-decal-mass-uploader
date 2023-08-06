7edbce4612926a293d15884c12e332de61929d2d
emppu <83163481+emppu-dev@users.noreply.github.com>
2023-08-06 16:43:37 +0300

    Made as simple as possible

--- setup.py
+++ setup.py
@@ -1,17 +1,4 @@
 import os
-import time
-import datetime
-
-packages = ["requests","urllib3","pillow"]
-
-def log(text):
-    timestamp = datetime.datetime.utcfromtimestamp(time.time()).strftime("%H:%M:%S")
-    print(f"[{timestamp}] {text}")
-
-for package in packages:
-    try:
-        __import__(package)
-        log(f"`{package}` is already installed")
-    except ImportError:
-        log(f"Installing `{package}`")
-        os.system(f"pip install {package}")
+os.system(f"pip install requests")
+os.system(f"pip install urllib3")
+os.system(f"pip install pillow")
