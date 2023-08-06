215e4b329b43d0064a795f70804f4feaf1fb1e30
emppu <83163481+emppu-dev@users.noreply.github.com>
2023-08-06 15:32:06 +0300

    Installs all needed libraries

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+import time
+import datetime
+
+packages = ["requests","urllib3","pillow"]
+
+def log(text):
+    timestamp = datetime.datetime.utcfromtimestamp(time.time()).strftime("%H:%M:%S")
+    print(f"[{timestamp}] {text}")
+
+for package in packages:
+    try:
+        __import__(package)
+        log(f"`{package}` is already installed")
+    except ImportError:
+        print(f"Installing `{package}`")
+        os.system(f"pip install {package}")
